4ea095afd48cffa8a7010f26cd9f005f3dc4256c
BangMingGing <privatebmk802@naver.com>
2023-12-05 14:47:24 +0900

    chore: Edit excution file name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 app_process = subprocess.Popen(['python3', '-m','uvicorn', 'app:app', '--reload', '--host=0.0.0.0', '--port=8889'])
-log_process = subprocess.Popen(['python3', 'MonitoringModule/loggingConsumer.py'])
+log_process = subprocess.Popen(['python3', 'MonitoringModule/consumer.py'])
 taskManager_process = subprocess.Popen(['python3', 'TaskManageModule/consumer.py'])
 
 app_process.wait()
