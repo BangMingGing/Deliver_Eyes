79327c1e7e0ed2773dc3a962f5cd427fcfb1f329
BangMingGing <96714217+BangMingGing@users.noreply.github.com>
2023-12-15 14:54:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import subprocess
-#/Users/chominguen/anaconda3/envs/Deliver_eyes/python
+
 app_process = subprocess.Popen(['python3', '-m','uvicorn', 'app:app', '--reload', '--host=0.0.0.0', '--port=8889'])
-# log_process = subprocess.Popen(['python3', 'MonitoringModule/consumer.py'])
-# taskManager_process = subprocess.Popen(['python3', 'TaskManageModule/consumer.py'])
 
 app_process.wait()
-# log_process.wait()
-# taskManager_process.wait()
