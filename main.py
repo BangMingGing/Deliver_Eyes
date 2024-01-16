3bb15fb1a6a6fbeb6b6a2dcc4d739d59e2efc4af
BangMingGing <privatebmk802@naver.com>
2024-01-16 14:49:56 +0900

    chore: Move web port to confie file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import subprocess
 
-app_process = subprocess.Popen(['python3', '-m','uvicorn', 'app:app', '--reload', '--host=0.0.0.0', '--port=8889'])
+from configuration import WEB_CONFIG
+
+app_process = subprocess.Popen(['python3', '-m','uvicorn', 'app:app', '--reload', '--host=0.0.0.0', f'--port={WEB_CONFIG.WEB_PORT}'])
 
 app_process.wait()
